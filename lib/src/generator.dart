fee8e04e2dd19aec0fc16c29d1657586e6129008
Netesh5 <netesh.paudel@gmail.com>
2025-07-24 10:23:38 +0545

    fix: replace hyphens with spaces in encoded text to improve formatting

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -1151,6 +1151,7 @@ class Generator {
         .replaceAll("´", "'")
         .replaceAll("»", '"')
         .replaceAll(" ", ' ')
+        .replaceAll("-", " ")
         .replaceAll("•", '.');
     if (!isKanji) {
       return codec.encode(text);
